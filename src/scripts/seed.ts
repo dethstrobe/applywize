7e27baaf979b38b8b0952a72b66c75886739d82a
dethstrobe <dethstrobe@gmail.com>
2025-07-13 16:06:21 -0400

    add seed file to populate the sqlite db

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,21 +1,28 @@
-import { defineScript } from "rwsdk/worker";
-import { db, setupDb } from "@/db";
-import { env } from "cloudflare:workers";
+import { defineScript } from "rwsdk/worker"
+import { db, setupDb } from "../db"
 
-export default defineScript(async () => {
-  await setupDb(env);
+export default defineScript(async ({ env }) => {
+  setupDb(env)
 
   await db.$executeRawUnsafe(`\
+    DELETE FROM Application;
+    DELETE FROM ApplicationStatus;
+    DELETE FROM Contact;
+    DELETE FROM Company;
+    DELETE FROM Credential;
     DELETE FROM User;
     DELETE FROM sqlite_sequence;
-  `);
+  `)
 
-  await db.user.create({
-    data: {
-      id: "1",
-      username: "testuser",
-    },
-  });
+  await db.applicationStatus.createMany({
+    data: [
+      { id: 1, status: "New" },
+      { id: 2, status: "Applied" },
+      { id: 3, status: "Interview" },
+      { id: 4, status: "Rejected" },
+      { id: 5, status: "Offer" },
+    ],
+  })
 
-  console.log("🌱 Finished seeding");
-});
+  console.log("🌱 Finished seeding")
+})
